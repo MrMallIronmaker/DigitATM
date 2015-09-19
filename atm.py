97418387aabc81d7270aaf16e9e1db1f4fb715b7
Milan <mdasgup2@illinois.edu>
2015-09-19 16:48:26 -0400

    Update

--- atm.py
+++ atm.py
@@ -1,28 +1,17 @@
 import requests
 import json
 
-customerId = '55e94a6af8d8770528e60d93'
 apiKey = '7705c20d5f3f8ecbbffc78c5cac2a2ff'
 
 while True:
+	customerId = '55e94a6cf8d8770528e618ca' #Fingerprint sensor decides
 	responseAction = {
 		201 : lambda : print('It worked'),
 		400 : lambda : print('Uh oh, something in your payload is wrong'),
 	}
 
-	url = 'http://api.reimaginebanking.com/customers/{}/accounts?key={}'.format(customerId,apiKey)
-	payload = {
-  	"type": "Savings",
-  	"nickname": "test",
-  	"rewards": 10000,
-  	"balance": 10000,	
-	}
+	url = 'http://api.reimaginebanking.com/accounts/{}?key={}'.format(customerId,apiKey)
 
-	req = requests.post(
-		url, 
-		data=json.dumps(payload),
-		headers={'content-type':'application/json'},
-		)
-
-	responseAction[req.status_code]()
+	req = requests.get(url)
+	print(req.json()['balance'])
 
