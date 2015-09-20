2a1752ae8450df19271d862f9aa4c53df2ba194b
MrMallIronmaker <725mrm@gmail.com>
2015-09-20 02:55:20 -0400

    now I can actually do fingerprint recognition

--- matlab/nxc_applied.m
+++ matlab/nxc_applied.m
@@ -3,17 +3,12 @@ function maxXC = nxc_applied(f1, f2)
 
 % compute cross-correlation
 xc = normxcorr2(f2, f1);
-xc2 = normxcorr2(f1, f2);
-
-%xcdiff = xc2 - rot90(xc,2);
 
 % weight by distance from center
 sigma = size(xc, 1) / 6;
 center_weight = size(xc, 1) * size(xc, 2) * 5;
 gauss = fspecial('gaussian', size(xc), sigma);
 xc = xc .* (1 + center_weight * gauss);
-
-figure, imagesc(xc .^ 3), colormap jet
 
 % Find maximum, compute transform coords
 [maxXC,ind] = max(xc(:));
--- matlab/print_match.m
+++ matlab/print_match.m
@@ -5,25 +5,29 @@ function id = cross_corr_tester()
 % possibly delay
 
 % find name of most recent image in C/images
-%d=dir('C:\Images\');
-d=dir('*.bmp');
+d=dir('C:\Images\*.bmp');
+%d=dir('*.bmp');
+if (length(d) == 0)
+    id = -1;
+    return
+end
 [dx,dx] = sort([d.datenum],'descend');
-newest = d(dx(1)).name
+newest = strcat('C:\Images\', d(dx(1)).name);
 
 % load it as an image
 needle = imread(newest);
 
 % load all images to compare it against
-d = dir('*.bmp');
+d = dir('fingers/*.bmp');
 correlations = zeros(length(d), 1);
 for i = (1:length(d))
-    hay = imread(d(i).name);
+    hay = imread(strcat('fingers/', d(i).name));
     % compare against images
     correlations(i) = nxc_applied(hay, needle);
 end
     
 % find number to return.
-best = 5.0;
+best = 7.0;
 bestIndex = 0;
 for i = (1 : length(correlations))
     if correlations(i) > best
